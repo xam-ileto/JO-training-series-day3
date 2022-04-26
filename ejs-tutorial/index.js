736d81c133c574d8786fe47eaa76f62bec18f47c
xam-ileto <xam.ileto@gmail.com>
2022-04-26 23:08:46 +0800

    added step 3 comment

--- ejs-tutorial/index.js
+++ ejs-tutorial/index.js
@@ -4,3 +4,10 @@ var app = express();
 const port = 3000;
 app.listen(port);
 console.log(`App listening at port ${port}`);
+
+// STEP 3: Set EJS as view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
